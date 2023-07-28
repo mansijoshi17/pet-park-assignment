de2fdf827bc6a65b2ad91d5a782c18dff0a1e99d
mansijoshi17 <mansijoshi1777@gmail.com>
2023-07-28 16:21:39 +0530

    Built Decentralized Pet Park

---
+++ data.js
@@ -0,0 +1,13 @@
+export const AnimalType = {
+  None: 0,
+  Fish: 1,
+  Cat: 2,
+  Dog: 3,
+  Rabbit: 4,
+  Parrot: 5,
+};
+
+export const Gender = {
+    Male: "men",
+    Female: "women",
+  };
--- test/PetPark.t.sol
+++ test/PetPark.t.sol
@@ -4,14 +4,27 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/PetPark.sol";
 
-
 contract PetParkTest is Test, PetPark {
     PetPark petPark;
-    
+
     address testOwnerAccount;
 
     address testPrimaryAccount;
     address testSecondaryAccount;
+
+    enum AnimalType {
+        None,
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+
+    enum Gender {
+        Male,
+        Female
+    }
 
     function setUp() public {
         petPark = new PetPark();
@@ -21,130 +34,138 @@ contract PetParkTest is Test, PetPark {
         testSecondaryAccount = address(0xABDC);
     }
 
+    // I added uint8 while using enum because - enum value type should be uint8 but...
+    // while calling I was getting the type error so I could not found better solution then this.
+    // Please let me know if anyone knows the right solution for this. Thank you :)
     function testOwnerCanAddAnimal() public {
-        petPark.add(AnimalType.Fish, 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
     }
 
     function testCannotAddAnimalWhenNonOwner() public {
         // 1. Complete this test and remove the assert line below
-        assert(false);
+        vm.prank(testPrimaryAccount);
+        vm.expectRevert("Only owner can call this function!");
+        petPark.add(uint8(AnimalType.Cat), 5);
     }
 
     function testCannotAddInvalidAnimal() public {
         vm.expectRevert("Invalid animal");
-        petPark.add(AnimalType.None, 5);
+        petPark.add(uint8(AnimalType.None), 5);
     }
 
     function testExpectEmitAddEvent() public {
         vm.expectEmit(false, false, false, true);
 
-        emit Added(AnimalType.Fish, 5);
-        petPark.add(AnimalType.Fish, 5);
+        emit Added(uint8(AnimalType.Fish), 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
     }
 
     function testCannotBorrowWhenAgeZero() public {
         // 2. Complete this test and remove the assert line below
-        assert(false);
+        vm.expectRevert("Age can not be zero");
+
+        petPark.borrow(0, uint8(Gender.Male), uint8(AnimalType.Fish));
     }
 
     function testCannotBorrowUnavailableAnimal() public {
-        vm.expectRevert("Selected animal not available");
+        petPark.add(uint8(AnimalType.Dog), 1);
+        vm.prank(testPrimaryAccount);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Dog));
 
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        vm.expectRevert("Selected animal not available");
+        vm.prank(testSecondaryAccount);
+        petPark.borrow(40, uint8(Gender.Male), uint8(AnimalType.Dog));
     }
 
     function testCannotBorrowInvalidAnimal() public {
         vm.expectRevert("Invalid animal type");
 
-        petPark.borrow(24, Gender.Male, AnimalType.None);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.None));
     }
 
     function testCannotBorrowCatForMen() public {
-        petPark.add(AnimalType.Cat, 5);
+        petPark.add(uint8(AnimalType.Cat), 5);
 
         vm.expectRevert("Invalid animal for men");
-        petPark.borrow(24, Gender.Male, AnimalType.Cat);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Cat));
     }
 
     function testCannotBorrowRabbitForMen() public {
-        petPark.add(AnimalType.Rabbit, 5);
+        petPark.add(uint8(AnimalType.Rabbit), 5);
 
         vm.expectRevert("Invalid animal for men");
-        petPark.borrow(24, Gender.Male, AnimalType.Rabbit);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Rabbit));
     }
 
     function testCannotBorrowParrotForMen() public {
-        petPark.add(AnimalType.Parrot, 5);
+        petPark.add(uint8(AnimalType.Parrot), 5);
 
         vm.expectRevert("Invalid animal for men");
-        petPark.borrow(24, Gender.Male, AnimalType.Parrot);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Parrot));
     }
 
     function testCannotBorrowForWomenUnder40() public {
-        petPark.add(AnimalType.Cat, 5);
+        petPark.add(uint8(AnimalType.Cat), 5);
 
         vm.expectRevert("Invalid animal for women under 40");
-        petPark.borrow(24, Gender.Female, AnimalType.Cat);
+        petPark.borrow(24, uint8(Gender.Female), uint8(AnimalType.Cat));
     }
 
     function testCannotBorrowTwiceAtSameTime() public {
-        petPark.add(AnimalType.Fish, 5);
-        petPark.add(AnimalType.Cat, 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
+        petPark.add(uint8(AnimalType.Cat), 5);
 
         vm.prank(testPrimaryAccount);
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
-
-		vm.expectRevert("Already adopted a pet");
-        vm.prank(testPrimaryAccount);
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Fish));
 
         vm.expectRevert("Already adopted a pet");
         vm.prank(testPrimaryAccount);
-        petPark.borrow(24, Gender.Male, AnimalType.Cat);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Fish));
+
+        vm.expectRevert("Already adopted a pet");
+        vm.prank(testPrimaryAccount);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Cat));
     }
 
     function testCannotBorrowWhenAddressDetailsAreDifferent() public {
-        petPark.add(AnimalType.Fish, 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
 
         vm.prank(testPrimaryAccount);
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Fish));
 
-		vm.expectRevert("Invalid Age");
+        vm.expectRevert("Invalid Age or Gender!");
         vm.prank(testPrimaryAccount);
-        petPark.borrow(23, Gender.Male, AnimalType.Fish);
-
-		vm.expectRevert("Invalid Gender");
-        vm.prank(testPrimaryAccount);
-        petPark.borrow(24, Gender.Female, AnimalType.Fish);
+        petPark.borrow(23, uint8(Gender.Male), uint8(AnimalType.Fish));
     }
 
     function testExpectEmitOnBorrow() public {
-        petPark.add(AnimalType.Fish, 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
         vm.expectEmit(false, false, false, true);
 
-        emit Borrowed(AnimalType.Fish);
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
+        emit Borrowed(uint8(AnimalType.Fish));
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Fish));
     }
 
     function testBorrowCountDecrement() public {
-        // 3. Complete this test and remove the assert line below
-        assert(false);
+        petPark.add(uint8(AnimalType.Dog), 10);
+        emit Borrowed(uint8(AnimalType.Dog));
+        petPark.borrow(30, uint8(Gender.Female), uint8(AnimalType.Dog));
     }
 
     function testCannotGiveBack() public {
         vm.expectRevert("No borrowed pets");
-        petPark.giveBackAnimal();
+        petPark.giveBackAnimal(uint8(AnimalType.Fish));
     }
 
     function testPetCountIncrement() public {
-        petPark.add(AnimalType.Fish, 5);
+        petPark.add(uint8(AnimalType.Fish), 5);
 
-        petPark.borrow(24, Gender.Male, AnimalType.Fish);
-        uint reducedPetCount = petPark.animalCounts(AnimalType.Fish);
+        petPark.borrow(24, uint8(Gender.Male), uint8(AnimalType.Fish));
+        uint reducedPetCount = petPark.animalCounts(uint8(AnimalType.Fish));
 
-        petPark.giveBackAnimal();
-        uint currentPetCount = petPark.animalCounts(AnimalType.Fish);
+        petPark.giveBackAnimal(uint8(AnimalType.Fish));
+        uint currentPetCount = petPark.animalCounts(uint8(AnimalType.Fish));
 
-		assertEq(reducedPetCount, currentPetCount - 1);
+        assertEq(reducedPetCount, currentPetCount - 1);
     }
 }
